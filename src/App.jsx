88be2225998cfb2536a20aa2292985ba057c6893
Baishali-7 <baishalima7@gmail.com>
2024-08-27 15:33:25 +0545

    navbar added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './layout.scss'
+import './App.css'
+import NavBar from './components/navbar/Navbar'
+
+function App() {
+  return (
+    <div className='layout' >
+      <NavBar></NavBar>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import './navbar.scss'
+
+function NavBar(){
+
+    return(
+        <nav>
+            <div className="left">left</div>
+            <div className="right">Right</div>
+        </nav>
+    )
+}
+
+export default NavBar;
