155e39038147e7fba4c9aaa960745c3d472b72a3
Baishali-7 <baishalima7@gmail.com>
2024-08-28 15:38:41 +0545

    added frontend

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,8 +4,20 @@ function NavBar(){
 
     return(
         <nav>
-            <div className="left">left</div>
-            <div className="right">Right</div>
+            <div className="left">
+                <a href='/' className='logo'>
+                    <img src ='/logo.png' alt=''></img>
+                    <span>Hiiiii</span>
+                </a>
+                <a href='/'>Home</a>
+                <a href='/'>About</a>
+                <a href='/'>Contact</a>
+                <a href='/'>Movies</a>
+            </div>
+            <div className="right">
+            <a href='/'>Sign In</a>
+            <a href='/'>Sign Up</a>
+            </div>
         </nav>
     )
 }
